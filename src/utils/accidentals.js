ced1e909fc33b2198a744a07833d4edb48dfd397
DavNej <davnej.dev@gmail.com>
2020-01-31 09:37:34 +0100

    remove htmlSymbol in accidentals

--- src/utils/accidentals.js
+++ src/utils/accidentals.js
@@ -1,20 +1,17 @@
 export const flat = new (function() {
   this.charcode = 9837;
-  this.htmlSymbol = '&#' + this.charcode + ';';
   this.jsSymbol = String.fromCharCode(parseInt(this.charcode));
   this.value = -1;
 })();
 
-export const sharp = new (function() {
-  this.charcode = 9839;
-  this.htmlSymbol = '&#' + this.charcode + ';';
-  this.jsSymbol = String.fromCharCode(parseInt(this.charcode));
-  this.value = 1;
-})();
-
 export const natural = new (function() {
   this.charcode = 8210;
-  this.htmlSymbol = '&#' + this.charcode + ';';
   this.jsSymbol = String.fromCharCode(parseInt(this.charcode));
   this.value = 0;
+})();
+
+export const sharp = new (function() {
+  this.charcode = 9839;
+  this.jsSymbol = String.fromCharCode(parseInt(this.charcode));
+  this.value = 1;
 })();
