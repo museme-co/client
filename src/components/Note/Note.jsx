dde50cb3016db7eaefd44d008730dbb41f9f0238
DavNej <davnej.dev@gmail.com>
2020-01-31 09:35:18 +0100

    style notes

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import './Note.scss';
 
 export default function Note({ name }) {
+  const className = name ? 'Note--active' : 'Note';
+
   return (
-    <div className="Note">
-      <span>{name}</span>
+    <div className="Fret">
+      <div className={className}>
+        <span>{name}</span>
+      </div>
     </div>
   );
 }
