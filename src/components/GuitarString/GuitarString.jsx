522a54c0607112e2064fe82743b1726000ce28d4
DavNej <davnej.dev@gmail.com>
2020-02-08 12:48:00 +0100

    add key to GuitarString to remove error

--- src/components/GuitarString/GuitarString.jsx
+++ src/components/GuitarString/GuitarString.jsx
@@ -8,8 +8,8 @@ export default function GuitarString({ stringNotes }) {
   return (
     <div className="GuitarString">
       <div className="GuitarString__stroke"></div>
-      {stringNotes.map(note => (
-        <Note key={note} name={note} />
+      {stringNotes.map((note, idx) => (
+        <Note key={`${note}-${idx}`} name={note} />
       ))}
     </div>
   );
